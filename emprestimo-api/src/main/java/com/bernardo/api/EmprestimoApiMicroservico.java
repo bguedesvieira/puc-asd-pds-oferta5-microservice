ddb2fc522300ce85119209c8cf594e12328e08ff
Bernardo <bvieira@gmail.com>
2016-08-16 22:54:59 -0300

    inclusao do projeto emprestimo-api

---
+++ emprestimo-api/src/main/java/com/bernardo/api/EmprestimoApiMicroservico.java
@@ -0,0 +1,13 @@
+package com.bernardo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprestimoApiMicroservico {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprestimoApiMicroservico.class, args);
+	}
+
+}
---
+++ emprestimo-api/src/main/java/com/bernardo/model/Objeto.java
@@ -0,0 +1,5 @@
+package com.bernardo.model;
+
+public class Objeto {
+
+}
